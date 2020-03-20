d9c37f0b3eff620fc2d879b72d0be4a2ad61b695
srankmeng <jaturong.duang@gmail.com>
2020-03-20 09:15:06 +0700

    Add crud product.template

--- my-modules/hospital/controllers/controllers.py
+++ my-modules/hospital/controllers/controllers.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+import json
 # from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
@@ -75,3 +76,58 @@ class Hospital(http.Controller):
             patients.append(vals)
         data = {'status': 200, 'response': patients, 'message': 'Done All Patients Returned'}
         return data
+
+# for test handheld app
+class Products(http.Controller):
+    @http.route('/get_products', type='json', auth='user')
+    def get_products(self):
+        print("Yes here enteredYes here enteredYes here enteredYes here enteredYes here entered")
+        products_rec = request.env['product.template'].search([])
+        products = []
+        for rec in products_rec:
+            vals = {
+                'id': rec.id,
+                'default_code': rec.default_code,
+                'name': rec.name,
+                'list_price': rec.list_price,
+                'standard_price': rec.standard_price,
+            }
+            products.append(vals)
+        data = {'status': 200, 'response': products, 'message': 'Success'}
+        return data
+
+    @http.route('/create_product', type='json', auth='user')
+    def create_product(self, **rec):
+        if request.jsonrequest:
+            if rec['name']:
+                vals = {
+                    'default_code': rec['default_code'],
+                    'name': rec['name'],
+                    'list_price': rec['list_price'],
+                    'standard_price': rec['standard_price'],
+                }
+                new_products= request.env['product.template'].sudo().create(vals)
+                args = {'success': True, 'message': 'Create Product Success', 'id': new_products.id}
+            return args
+
+    @http.route('/update_product', type='json', auth='user')
+    def update_product(self, **rec):
+        if request.jsonrequest:
+            if rec['id']:
+                product = request.env['product.template'].sudo().search([('id', '=', rec['id'])])
+                if product:
+                    product.sudo().write(rec)
+                args = {'success': True, 'message': 'Update Product Success'}
+        return args
+
+    @http.route('/delete_product', type='json', auth='user')
+    def delete_product(self, **rec):
+        if request.jsonrequest:
+            if rec['id']:
+                product = request.env['product.template'].sudo().search([('id', '=', rec['id'])])
+                if product:
+                    product.unlink()
+                args = {'success': True, 'message': 'Delete Product Success'}
+        return args
+
+ 
